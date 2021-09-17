d529676cb6625b586f720b8cf053a10dccdba9d3
mayrinarahmi <auliamayrina2014@gmail.com>
2021-09-17 16:24:54 +0700

    first

---
+++ kain.php
@@ -0,0 +1,66 @@
+ <!-- KAIN -->
+ <section class="kain section" id="kain">
+                 <h2 class="section-title">Choose Your Sasirangan</h2>
+
+                 <div class="kain__container container grid">
+                     <!-- kain1 -->
+                     <div class="kain__card">
+                         <img src="assets/img/kain1.jfif" alt="" class="kain__img">
+                         <div class="kain__content">
+                             <span class="kain__rating">
+                                <i class='bx bx-star'></i>
+                                <span class="data__rating-number">4,8</span>
+                             </span>
+                             <div class="kain__data">
+                                 <h3 class="kain__title">Sasirangan Motif Daun</h3>
+                                 <span class="kain__price">Rp.150.000</span>
+                             </div> 
+                         </div>
+                     </div>
+
+                      <!-- kain1 -->
+                      <div class="kain__card">
+                        <img src="assets/img/kain2.jfif" alt="" class="kain__img">
+                        <div class="kain__content">
+                            <span class="kain__rating">
+                               <i class='bx bx-star'></i>
+                               <span class="data__rating-number">4,8</span>
+                            </span>
+                            <div class="kain__data">
+                                <h3 class="kain__title">Sasirangan Motif Daun</h3>
+                                <span class="kain__price">Rp.150.000</span>
+                            </div> 
+                        </div>
+                    </div>
+
+                     <!-- kain1 -->
+                     <div class="kain__card">
+                        <img src="assets/img/kain3.jfif" alt="" class="kain__img">
+                        <div class="kain__content">
+                            <span class="kain__rating">
+                               <i class='bx bx-star'></i>
+                               <span class="data__rating-number">4,8</span>
+                            </span>
+                            <div class="kain__data">
+                                <h3 class="kain__title">Sasirangan Motif Daun</h3>
+                                <span class="kain__price">Rp.150.000</span>
+                            </div> 
+                        </div>
+                    </div>
+
+                     <!-- kain1 -->
+                     <div class="kain__card">
+                        <img src="assets/img/kain4.jfif" alt="" class="kain__img">
+                        <div class="kain__content">
+                            <span class="kain__rating">
+                               <i class='bx bx-star'></i>
+                               <span class="data__rating-number">4,8</span>
+                            </span>
+                            <div class="kain__data">
+                                <h3 class="kain__title">Sasirangan Motif Daun</h3>
+                                <span class="kain__price">Rp.150.000</span>
+                            </div> 
+                        </div>
+                    </div>
+                 </div>
+             </section>
